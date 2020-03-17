c5bfcb11a6e79fd23f324120598693419858f7b3
Daniel Kersten <dkersten1994@gmail.com>
2020-03-17 11:42:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,7 @@
 def roll_call_dwarves(array) # code an argument here
   # Your code here
-  puts array.each_with_index { |item, index| puts "#{index} #{item}" }
+  index = 1
+  puts array.each_with_index { |item, index| puts "#{index}. #{item}" }
 end
 
 def summon_captain_planet() # code an argument here
