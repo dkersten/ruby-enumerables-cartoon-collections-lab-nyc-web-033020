ae74f1efb16e48db8e45bd907c07c927bd10a7b8
Daniel Kersten <dkersten1994@gmail.com>
2020-03-17 11:45:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,15 +5,15 @@ def roll_call_dwarves(array) # code an argument here
     puts "#{index}. #{item}"
 end
 
-def summon_captain_planet() # code an argument here
-  # Your code here
-end
+# def summon_captain_planet() # code an argument here
+#   # Your code here
+# end
 
-def long_planeteer_calls() # code an argument here
-  # Your code here
-end
+# def long_planeteer_calls() # code an argument here
+#   # Your code here
+# end
 
-def find_the_cheese() # code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+# def find_the_cheese() # code an argument here
+#   # the array below is here to help
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+# end
